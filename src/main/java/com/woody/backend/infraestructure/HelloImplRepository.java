8e035c131ab613eb031be39833c8ab7691dd5320
NicholasAstor <nicholas.barbosa@edu.pucrs.br>
2025-12-24 17:58:38 -0300

    feat:adding webhook to discord when a PR is opened

--- src/main/java/com/woody/backend/infraestructure/HelloImplRepository.java
+++ src/main/java/com/woody/backend/infraestructure/HelloImplRepository.java
@@ -2,22 +2,21 @@ package com.woody.backend.infraestructure;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
 import com.woody.backend.infraestructure.entity.Hello;
 import com.woody.backend.infraestructure.repository.interfaces.HelloRepository;
 import com.woody.backend.infraestructure.repository.interfacesJPA.HelloRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public class HelloImplRepository implements HelloRepository{
+public class HelloImplRepository implements HelloRepository {
     @Autowired
     private HelloRepositoryJPA helloRepo;
 
-    public HelloImplRepository(){}
+    public HelloImplRepository() {}
 
     @Override
-    public List<Hello> get(){
+    public List<Hello> get() {
         return this.helloRepo.findAll();
     }
 }
